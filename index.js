d888409a116f674b2fedc7ce30ed66f3673a231f
alalade191 <alalade191@gmail.com>
2022-11-14 12:00:57 +0100

    lattest commit

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
+let counter = document.querySelector("#counter");
+let increament = document.querySelector("#increament");
+let decreament = document.querySelector("#decreament");
+let save = document.querySelector("#save");
+let entries = document.querySelector("#entries");
+let count = 0;
 
- let counter = document.querySelector('#counter');
- let increament = document.querySelector('#increament');
- let decreament = document.querySelector('#decreament');
- let save = document.querySelector('#save');
- let entries = document.querySelector('#entries');
- let count = 0;
- 
-
-
-increament.addEventListener('click', () => {
- count += 1
- counter.textContent = count;
-
-})
-
-decreament.addEventListener('click', () => {
- count -= 1
+increament.addEventListener("click", () => {
+  count += 1;
   counter.textContent = count;
-})
+});
 
-save.addEventListener('click', () => {
- let result = count + ' - '
- entries.textContent += result
- count = 0;
- 
-})
+decreament.addEventListener("click", () => {
+  count -= 1;
+  counter.textContent = count;
+  if (count < 0) {
+    counter.textContent = "Execution stop here!";
+  }
+});
 
-
-
+save.addEventListener("click", () => {
+  result = count + " - ";
+  entries.textContent += result;
+  count = 0;
+});
